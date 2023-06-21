235b653de4af912eacad8e2d5d2a11b99387f083
Vukan Mirkovic <vukan@dotlab.rs>
2023-06-21 20:07:43 +0200

    Add LogBox ignoreLogs for non-serializable values

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { LogBox, SafeAreaView } from "react-native";
 import CustomTabBar from "./src/components/CustomTabBar";
 import CategoryStack from "./src/navigation/CategoryStack";
 import MainStack from "./src/navigation/MainStack";
 import { flex } from "./src/styles";
 import { MainTabParamList } from "./src/types";
+
+LogBox.ignoreLogs(["Non-serializable values were found in the navigation state."]);
 
 const Tab = createBottomTabNavigator<MainTabParamList>();
 
